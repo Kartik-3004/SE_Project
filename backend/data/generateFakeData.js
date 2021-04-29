fbaaec2770888a32575ff9a283385b21134b71c2
manavgopal <gopal.1@iitj.ac.in>
2021-04-29 14:57:16 +0530

    added reservationSchema file

--- backend/data/generateFakeData.js
+++ backend/data/generateFakeData.js
@@ -1,11 +1,11 @@
-// Randomly generate a fake allTables JSON file
+// Randomly generate a fake allRooms JSON file
 
 const fs = require("fs");
-const numRooms = Math.floor(Math.random() * 10) + 16; // 16 - 26 (exclusive)
+const numRooms = Math.floor(Math.random() * 10) + 10; // 10 - 20 (exclusive)
 
 let fakeRooms = [];
 for (i = 1; i < numRooms; i++) {
-  const chairs = Math.floor(Math.random() * 600) + 40; // 2-8 (exclusive)
+  const chairs = Math.floor(Math.random() * 600) + 40; // 40-600 (exclusive)
   const name = `Room ${i}`;
   // const availability = [true, false][Math.round(Math.random())];
   const location = ["Ground floor", "1st floor", "2nd floor"][Math.floor(Math.random() * 3)]; // 0-3 (exclusive)
