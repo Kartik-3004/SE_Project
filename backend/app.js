75181cb28183e22a2580f49941307fa56a6d83d2
Kartik-3004 <narayan.2@iitj.ac.in>
2021-05-02 22:47:26 +0530

    Integrate Database

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,31 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-const indexRouter = require('./routes/index');
+//MongoDB
+mongoose.connect(process.env.MONGO_URL, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+});
+const db = mongoose.connection;
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-ß;
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+//Routes
+app.use('/', require('./routes/reservationRoute'));
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', (_) => {
+	console.log('Connected to DB');
+});
 
 module.exports = app;
