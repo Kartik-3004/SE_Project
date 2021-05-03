70544d507f0067a9d7e08e3415f1664feca91166
Kartik-3004 <narayan.2@iitj.ac.in>
2021-05-03 11:16:17 +0530

    Add route

--- backend/routes/reservationRoute.js
+++ backend/routes/reservationRoute.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-	res.status(200).send('All working');
+const Day = require('../models/day').model;
+const Reservation = require('../models/reservation').model;
+
+// Parameters:
+// {
+//   "date": String ("Dec 02 2019 06:00"),
+//   "table": table id,
+// 	"name": String,
+// 	"phone": String,
+// 	"email": String
+// }
+
+router.post('/', function (req, res, next) {
+	Day.find({ date: req.body.date }, (err, days) => {
+		if (!err) {
+			if (days.length > 0) {
+				let day = days[0];
+				day.rooms.forEach((table) => {
+					if (room._id == req.body.room) {
+						// The correct table is table
+						room.reservation = new Reservation({
+							name: req.body.name,
+							phone: req.body.phone,
+							email: req.body.email,
+						});
+						room.isAvailable = false;
+						day.save((err) => {
+							if (err) {
+								console.log(err);
+							} else {
+								console.log('Reserved');
+								res.status(200).send('Added Reservation');
+							}
+						});
+					}
+				});
+			} else {
+				console.log('Day not found');
+			}
+		}
+	});
 });
 
 module.exports = router;
